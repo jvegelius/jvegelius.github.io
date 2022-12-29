ce6b9a50afaa3d0d757ec75a3ed2bf7170fa155b
lukas <lukas.arnroth@gmail.com>
2022-12-29 16:13:27 +0100

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './App.css';
 import { Card } from './Components/Card/Card';
 import Body from './Components/Body/Body';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className='grid justify-items-stretch grid-cols-1 laptop:grid-cols-6'>
-      <div></div>
-      <div>
-        <Card />
+    <>
+      <div className='grid justify-items-stretch grid-cols-1 laptop:grid-cols-6 pb-10'>
+        <div></div>
+        <div>
+          <Card />
+        </div>
+        <div className='laptop:col-span-3'>
+          <Body />
+        </div>
       </div>
-      <div className='laptop:col-span-3'>
-        <Body />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
